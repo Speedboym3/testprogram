1e0990a5f6b6470138dc2820f3a6fae37bcd4742
Qiuyang Zong <2874620865@qq.com>
2020-12-14 13:56:44 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1></h1>
+  <h1>This is the Vue template.</h1>
+  <HelloWorld msg="This is the HelloWorld compnent!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
